7779ced29fc7e37856cc75cc7b989b32d427768b
jjvega86 <jj.vega86@gmail.com>
2020-08-14 10:25:47 -0400

    Added a ValidateInput method to Human class to validate gesture input.

--- RPSLS/Human.cs
+++ RPSLS/Human.cs
@@ -22,7 +22,30 @@ namespace RPSLS
             Console.WriteLine($"Please choose your gesture, {name}!");
             gestureChoice = Console.ReadLine();
             Console.WriteLine("");
+            ValidateInput(gestureChoice);
             return gestureChoice;
+        }
+
+        public void ValidateInput(string input)
+        {
+            bool inputValid = false;
+
+            for (int i = 0; i < gestures.Count; i++)
+            {
+                if (gestures[i].name == input)
+                {
+                    inputValid = true;
+                }
+               
+            }
+
+            if (inputValid == false)
+            {
+                Console.WriteLine("I'm sorry, I don't recognize that gesture.");
+                Console.WriteLine("Please enter your gesture exactly as you see it!");
+                AssignGesture();
+            }
+
         }
 
     }
